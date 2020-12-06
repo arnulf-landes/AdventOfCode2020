d58f14432b29d5cd7396d4dd4e2d84cb09f38501
Arnulf Landes <75379107+arnulf-landes@users.noreply.github.com>
2020-12-06 12:25:15 +0100

    removed leftover commented-out code

--- day06/part2.js
+++ day06/part2.js
@@ -7,10 +7,3 @@ const group_answers = answers.map(group => group.map(person => person.split(''))
 console.log(
   group_answers.map(group => group.length).reduce((acc, val) => +acc + val)
 );
-
-/*
-map(group => group.split('').reduce((acc, val) => {
-  acc[val] = 1;
-  return acc;
-}, {}));
-*/
